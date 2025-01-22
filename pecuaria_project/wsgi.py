a76828009ad6895ae2705d2d060edff971cc5438
DevAppsAagro <devapp.apps@gmail.com>
2025-01-21 23:35:25 -0300

    Add Vercel configuration files and update wsgi.py for Vercel deployment

--- pecuaria_project/wsgi.py
+++ pecuaria_project/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pecuaria_project.settings')
 
 application = get_wsgi_application()
+app = application  # Add this for Vercel
