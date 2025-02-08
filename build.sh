76e81056547efd2d1a414a241db2686d76f77b18
DevAppsAagro <devapp.apps@gmail.com>
2025-02-08 15:46:28 -0300

    fix: Simplificado script de build
    - Removido comando migrate desnecessário
    - Mantido apenas pip install e collectstatic

--- build.sh
+++ build.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Instala as dependências
 pip install -r requirements.txt
-
-# Coleta arquivos estáticos
 python3.9 manage.py collectstatic --noinput
-
-# Aplica as migrações
-python manage.py migrate
